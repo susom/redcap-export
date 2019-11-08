86fe2fe12cbb62ca651e19774c05d4af5bb0c2b7
Ihab Zeedia <ihab.zeedia@gmail.cm>
2019-11-08 15:40:24 -0800

    initial commit for export tool

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # redcap-export
-Python script to export REDCap project data via API using PyCap
+Python script to export REDCap project data via API using PyCap package
+
+Installation
+------------
+ 
+
+    ``$ pip install -r requirements.txt``
+
+Configuration
+-------------
+
+define json object for each project you want to export. 
+
+```
+{
+      "token" : "[API_PROJECT_TOKEN]",
+      "path" : "[PATH_WHERE_TO_SAVE_FILE]",
+      "layout" : "[FOR_DROPDOWN_AND_CHECKBOXES_SPECIFY_RAW_OR_LABEL]"
+}
+```
---
+++ export.py
@@ -0,0 +1,30 @@
+import json
+import redcap
+import time
+#define api main url
+URL = "http://ihabz.stanford.edu/api/"
+
+ts = time.time()
+#load json file contains configuration.
+with open('config.json') as json_file:
+    data = json.load(json_file)
+
+##loop over the configuration.
+for p in data['projects']:
+    token = p['token']
+    path = p['path']
+    layout = p['layout']
+    #if layout not defined or has wrong value then its `raw`
+    if layout == '' or (layout != 'raw' and layout != 'label'):
+        layout = 'raw'
+
+    #initiate REDCap api object
+    project = redcap.Project(URL, token)
+
+    #export all records as csv string format
+    all_data = project.export_records(format='csv', raw_or_label=layout.lower())
+
+    #open file handler
+    with open(path + 'projects_' + str(ts) + '.csv', 'w') as writeFile:
+        writeFile.write(all_data)
+    writeFile.close()
