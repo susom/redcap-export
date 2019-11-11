faf0a8909fb3c58ddfbaa9d3145541cae30e96fa
Ihab Zeedia <ihab.zeedia@gmail.cm>
2019-11-11 09:39:56 -0800

    change logic to OOP.

--- export.py
+++ export.py
@@ -1,30 +1,39 @@
 import json
 import redcap
 import time
-#define api main url
-URL = "https://redcap.stanford.edu//api/"
 
-ts = time.time()
-#load json file contains configuration.
-with open('config.json') as json_file:
-    data = json.load(json_file)
 
-##loop over the configuration.
-for p in data['projects']:
-    token = p['token']
-    path = p['path']
-    layout = p['layout']
-    #if layout not defined or has wrong value then its `raw`
-    if layout == '' or (layout != 'raw' and layout != 'label'):
-        layout = 'raw'
+# initiate the class
+class Export:
+    def process(self):
+        # define api main url
+        URL = "https://redcap.stanford.edu/api/"
 
-    #initiate REDCap api object
-    project = redcap.Project(URL, token)
+        ts = time.time()
+        # load json file contains configuration.
+        with open('config.json') as json_file:
+            data = json.load(json_file)
 
-    #export all records as csv string format
-    all_data = project.export_records(format='csv', raw_or_label=layout.lower())
+        ##loop over the configuration.
+        for p in data['projects']:
+            token = p['token']
+            path = p['path']
+            layout = p['layout']
+            # if layout not defined or has wrong value then its `raw`
+            if layout == '' or (layout != 'raw' and layout != 'label'):
+                layout = 'raw'
 
-    #open file handler
-    with open(path + 'projects_' + str(ts) + '.csv', 'w') as writeFile:
-        writeFile.write(all_data)
-    writeFile.close()
+            # initiate REDCap api object
+            project = redcap.Project(URL, token)
+
+            # export all records as csv string format
+            all_data = project.export_records(format='csv', raw_or_label=layout.lower())
+
+            # open file handler
+            with open(path + 'projects_' + str(ts) + '.csv', 'w') as writeFile:
+                writeFile.write(all_data)
+            writeFile.close()
+
+
+export = Export()
+export.process()
